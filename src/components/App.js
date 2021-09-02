25f82c47ae11c60e320af26c553f8ff2dc130a39
Brian <brianjmorrissey@gmail.com>
2021-09-02 15:56:01 -0500

    react-router added

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import ShowChord from './ShowChord';
 import Navbar from './Navbar';
 import {connect} from 'react-redux';
 import TuningDropdown from './TuningDropdown';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const showCalc = () => {
-  if (window.location.pathname === '/capo_calculator/') {
     return <ChordConversion />
+  
   }
-}
+
 
 const showChord = () => {
   if (window.location.pathname === '/show_chord') {
@@ -25,11 +25,20 @@ const capoCheck = () => {
   }
 }
 
-
 const App = (props) => {
-
+  
+  console.log(capoCheck)
    return (
     <div>
+    <div>
+      <BrowserRouter>
+      <Route path="/" exact component={showCalc} />
+      <Route path="/capo_calculator" exact component={showCalc} />
+      <Route path="/capo_check" exact component={showCalc} />
+
+      </BrowserRouter> 
+    </div>
+
         <Navbar />
          {showCalc()}
         {/* {showChord()} */}
